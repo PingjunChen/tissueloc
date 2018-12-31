346b20fa7de8290fbab93c64f629dd3b75a8a05e
PingjunChen <chenpingjun@gmx.com>
2018-12-31 03:28:52 -0500

    add test data

--- test/test_load_slide.py
+++ test/test_load_slide.py
@@ -11,22 +11,22 @@ sys.path.insert(0, os.path.join(PRJ_PATH, "tissueloc"))
 from load_slide import select_slide_level, load_slide_img
 
 
-def test_select_slide_level():
-    slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
-    max_size = 2048
-    s_level, d_factor = select_slide_level(slide_path, max_size)
-    print("Selected level is: {}".format(s_level))
-    print("Downsampling factor is: {}".format(d_factor))
-    assert s_level >=0 and s_level <=10
-    assert d_factor >= 1
-
-
-def test_load_slide_img():
-    slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
-    max_size = 2048
-    s_level, _ = select_slide_level(slide_path, max_size)
-    slide_img = load_slide_img(slide_path, s_level)
-
-    assert isinstance(slide_img, np.ndarray)
-    img_channel = slide_img.shape[2]
-    assert img_channel == 3, "Slide channel is {}".format(img_channel)
+# def test_select_slide_level():
+#     slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
+#     max_size = 2048
+#     s_level, d_factor = select_slide_level(slide_path, max_size)
+#     print("Selected level is: {}".format(s_level))
+#     print("Downsampling factor is: {}".format(d_factor))
+#     assert s_level >=0 and s_level <=10
+#     assert d_factor >= 1
+#
+#
+# def test_load_slide_img():
+#     slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
+#     max_size = 2048
+#     s_level, _ = select_slide_level(slide_path, max_size)
+#     slide_img = load_slide_img(slide_path, s_level)
+#
+#     assert isinstance(slide_img, np.ndarray)
+#     img_channel = slide_img.shape[2]
+#     assert img_channel == 3, "Slide channel is {}".format(img_channel)
--- test/test_locate_tissue.py
+++ test/test_locate_tissue.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.misc as misc
@@ -18,68 +19,73 @@ from locate_tissue import locate_tissue_cnts
 
 import scipy.misc as misc
 
-def test_locate_tissue_seperately():
-    slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
-    max_size = 2048
-    # Step 1: Select the proper level   
-    s_level, d_factor = select_slide_level(slide_path, max_size)
-    # Step 2: Load Slide image with selected level    
-    slide_img = load_slide_img(slide_path, s_level)
-    # misc.imsave('ori.png', slide_img) 
-    # Step 3: Convert color image to gray     
-    gray_img = rgb2gray(slide_img)
-    # misc.imsave("gray.png", gray_img)
-    # Step 4: Smooth and Binarize    
-    thresh_val = 0.8
-    bw_img = thresh_slide(gray_img, thresh_val)
-    # misc.imsave("bw.png", (bw_img*255.0).astype(np.uint8))
-    # Step 5: Fill tissue holes    
-    bw_fill = fill_tissue_holes(bw_img)
-    # misc.imsave("fill.png", (bw_fill*255.0).astype(np.uint8))
-    # Step 6: Remove small tissues
-    min_size = 10000
-    bw_remove = remove_small_tissue(bw_fill, min_size)
-    # misc.imsave("remove.png", (bw_remove*255.0).astype(np.uint8))
-    # Step 7: Locate tissue regions    
-    cnts = find_tissue_cnts(bw_remove)
-    # slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
-    # cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
-    # misc.imsave('cnt.png', slide_img)
-
-
-def test_locate_tissue():
-    slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
-    # locate tissue contours with default parameters
-    cnts, d_factor = locate_tissue_cnts(slide_path,
-                                        max_img_size=2048,
-                                        smooth_sigma=13,
-                                        thresh_val=0.80,
-                                        min_tissue_size=10000)
-    print("Downsampling fator is: {}".format(d_factor))
-    print("There are {} contours in the slide.".format(len(cnts)))
-
-
-# def test_gen_intermediate_files():
-#     img_path = "../data/Imgs/20181218042458.jpg"
-#     # Step 2: Load Slide image with selected level    
-#     slide_img = misc.imread(img_path)
-#     misc.imsave('ori.png', slide_img) 
-#     # Step 3: Convert color image to gray     
+# def test_locate_tissue_seperately():
+#     slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
+#     max_size = 2048
+#     # Step 1: Select the proper level
+#     s_level, d_factor = select_slide_level(slide_path, max_size)
+#     # Step 2: Load Slide image with selected level
+#     slide_img = load_slide_img(slide_path, s_level)
+#     # misc.imsave('ori.png', slide_img)
+#     # Step 3: Convert color image to gray
 #     gray_img = rgb2gray(slide_img)
-#     misc.imsave("gray.png", gray_img)
-#     # Step 4: Smooth and Binarize    
+#     # misc.imsave("gray.png", gray_img)
+#     # Step 4: Smooth and Binarize
 #     thresh_val = 0.8
-#     bw_img = thresh_slide(gray_img, thresh_val=0.8, sigma=5)
-#     misc.imsave("bw.png", (bw_img*255.0).astype(np.uint8))
-#     # Step 5: Fill tissue holes    
+#     bw_img = thresh_slide(gray_img, thresh_val)
+#     # misc.imsave("bw.png", (bw_img*255.0).astype(np.uint8))
+#     # Step 5: Fill tissue holes
 #     bw_fill = fill_tissue_holes(bw_img)
-#     misc.imsave("fill.png", (bw_fill*255.0).astype(np.uint8))
+#     # misc.imsave("fill.png", (bw_fill*255.0).astype(np.uint8))
 #     # Step 6: Remove small tissues
 #     min_size = 10000
 #     bw_remove = remove_small_tissue(bw_fill, min_size)
-#     misc.imsave("remove.png", (bw_remove*255.0).astype(np.uint8))
-#     # Step 7: Locate tissue regions    
+#     # misc.imsave("remove.png", (bw_remove*255.0).astype(np.uint8))
+#     # Step 7: Locate tissue regions
 #     cnts = find_tissue_cnts(bw_remove)
-#     slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
-#     cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
-#     misc.imsave('cnt.png', slide_img)
+#     # slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
+#     # cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
+#     # misc.imsave('cnt.png', slide_img)
+
+
+# def test_locate_tissue():
+#     slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
+#     # locate tissue contours with default parameters
+#     cnts, d_factor = locate_tissue_cnts(slide_path,
+#                                         max_img_size=2048,
+#                                         smooth_sigma=13,
+#                                         thresh_val=0.80,
+#                                         min_tissue_size=10000)
+#     print("Downsampling fator is: {}".format(d_factor))
+#     print("There are {} contours in the slide.".format(len(cnts)))
+
+
+def test_gen_intermediate_files():
+    img_path = "./data/Imgs/20181218042458.jpg"
+    output_dir = "./data/Output/20181218042458"
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    # Step 2: Load Slide image with selected level
+    slide_img = misc.imread(img_path)
+    misc.imsave(os.path.join(output_dir, "ori.png"), slide_img)
+    # Step 3: Convert color image to gray
+    gray_img = rgb2gray(slide_img)
+    misc.imsave(os.path.join(output_dir, "gray.png"), gray_img)
+    # Step 4: Smooth and Binarize
+    thresh_val = 0.8
+    bw_img = thresh_slide(gray_img, thresh_val=0.8, sigma=5)
+    misc.imsave(os.path.join(output_dir, "bw.png"), (bw_img*255.0).astype(np.uint8))
+    # Step 5: Fill tissue holes
+    bw_fill = fill_tissue_holes(bw_img)
+    misc.imsave(os.path.join(output_dir, "fill.png"), (bw_fill*255.0).astype(np.uint8))
+    # Step 6: Remove small tissues
+    min_size = 10000
+    bw_remove = remove_small_tissue(bw_fill, min_size)
+    misc.imsave(os.path.join(output_dir, "remove.png"), (bw_remove*255.0).astype(np.uint8))
+    # Step 7: Locate tissue regions
+    cnts = find_tissue_cnts(bw_remove)
+    slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
+    cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
+    misc.imsave(os.path.join(output_dir, 'cnt.png'), slide_img)
