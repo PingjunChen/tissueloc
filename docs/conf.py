dc6f0a7ea7e7eea6b6b4e13c953dc7edc0eb2406
PingjunChen <chenpingjun@gmx.com>
2019-05-12 00:46:50 -0400

    update version to 2.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Pingjun Chen'
 # The short X.Y version
 version = '2.0'
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
