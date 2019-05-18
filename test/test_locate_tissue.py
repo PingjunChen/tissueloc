b409b0ec9accdc1fe371111cd2f0b1f1020db9a0
PingjunChen <chenpingjun@gmx.com>
2019-05-18 02:01:51 -0400

    replace scipy with skimage

--- test/test_locate_tissue.py
+++ test/test_locate_tissue.py
@@ -3,7 +3,6 @@
 import os, sys
 import shutil
 import numpy as np
-import scipy.misc as misc
 from skimage import io
 import cv2
 
@@ -31,23 +30,23 @@ def test_gen_intermediate_files():
     io.imsave(os.path.join(output_dir, "ori.png"), slide_img)
     # Step 3: Convert color image to gray
     gray_img = rgb2gray(slide_img)
-    misc.imsave(os.path.join(output_dir, "gray.png"), gray_img)
+    io.imsave(os.path.join(output_dir, "gray.png"), gray_img)
     # Step 4: Smooth and Binarize
     thresh_val = 0.8
     bw_img = thresh_slide(gray_img, thresh_val=thresh_val, sigma=5)
-    misc.imsave(os.path.join(output_dir, "bw.png"), (bw_img*255.0).astype(np.uint8))
+    io.imsave(os.path.join(output_dir, "bw.png"), (bw_img*255.0).astype(np.uint8))
     # Step 5: Fill tissue holes
     bw_fill = fill_tissue_holes(bw_img)
-    misc.imsave(os.path.join(output_dir, "fill.png"), (bw_fill*255.0).astype(np.uint8))
+    io.imsave(os.path.join(output_dir, "fill.png"), (bw_fill*255.0).astype(np.uint8))
     # Step 6: Remove small tissues
     min_size = 10000
     bw_remove = remove_small_tissue(bw_fill, min_size)
-    misc.imsave(os.path.join(output_dir, "remove.png"), (bw_remove*255.0).astype(np.uint8))
+    io.imsave(os.path.join(output_dir, "remove.png"), (bw_remove*255.0).astype(np.uint8))
     # Step 7: Locate tissue regions
     cnts = find_tissue_cnts(bw_remove)
     slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
     cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
-    misc.imsave(os.path.join(output_dir, 'cnt.png'), slide_img)
+    io.imsave(os.path.join(output_dir, 'cnt.png'), slide_img)
 
 # def test_locate_tissue_seperately():
 #     slide_path = "../data/SoftTissue/TCGA-B9EB312E82F6.svs"
@@ -56,26 +55,26 @@ def test_gen_intermediate_files():
 #     s_level, d_factor = select_slide_level(slide_path, max_size)
 #     # Step 2: Load Slide image with selected level
 #     slide_img = load_slide_img(slide_path, s_level)
-#     # misc.imsave('ori.png', slide_img)
+#     # io.imsave('ori.png', slide_img)
 #     # Step 3: Convert color image to gray
 #     gray_img = rgb2gray(slide_img)
-#     # misc.imsave("gray.png", gray_img)
+#     # io.imsave("gray.png", gray_img)
 #     # Step 4: Smooth and Binarize
 #     thresh_val = 0.8
 #     bw_img = thresh_slide(gray_img, thresh_val)
-#     # misc.imsave("bw.png", (bw_img*255.0).astype(np.uint8))
+#     # io.imsave("bw.png", (bw_img*255.0).astype(np.uint8))
 #     # Step 5: Fill tissue holes
 #     bw_fill = fill_tissue_holes(bw_img)
-#     # misc.imsave("fill.png", (bw_fill*255.0).astype(np.uint8))
+#     # io.imsave("fill.png", (bw_fill*255.0).astype(np.uint8))
 #     # Step 6: Remove small tissues
 #     min_size = 10000
 #     bw_remove = remove_small_tissue(bw_fill, min_size)
-#     # misc.imsave("remove.png", (bw_remove*255.0).astype(np.uint8))
+#     # io.imsave("remove.png", (bw_remove*255.0).astype(np.uint8))
 #     # Step 7: Locate tissue regions
 #     cnts = find_tissue_cnts(bw_remove)
 #     # slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
 #     # cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 9)
-#     # misc.imsave('cnt.png', slide_img)
+#     # io.imsave('cnt.png', slide_img)
 
 
 # def test_locate_tissue():
