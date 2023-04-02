3c3e9196e3927c9d8e1ff445408825f18c529e9b
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-04-02 18:56:34 +0200

    Improved speed and optimized code readability

--- tissueloc/locate_tissue.py
+++ tissueloc/locate_tissue.py
@@ -11,34 +11,26 @@ from .load_slide import select_slide_level, load_slide_img
 
 def rgb2gray(img):
     """Convert RGB image to gray space.
-    Parameters
-    ----------
-    img : np.array
-        RGB image with 3 channels.
-    Returns
-    -------
-    gray: np.array
-        Gray image
-    """
-    gray = np.dot(img, [0.299, 0.587, 0.114])
 
-    return gray
+    Args:
+        img (np.ndarray): RGB image with 3 channels.
+
+    Returns:
+        np.ndarray: Gray image
+    """
+    return np.dot(img, [0.299, 0.587, 0.114])
 
 
 def thresh_slide(gray, thresh_val, sigma=13):
-    """ Threshold gray image to binary image
-    Parameters
-    ----------
-    gray : np.array
-        2D gray image.
-    thresh_val: float
-        Thresholding value.
-    smooth_sigma: int
-        Gaussian smoothing sigma.
-    Returns
-    -------
-    bw_img: np.array
-        Binary image
+    """ Threshold gray image to binary image.
+
+    Args:
+        gray (np.ndarray): 2D gray image.
+        thresh_val (float): Thresholding value.
+        smooth_sigma (int): Gaussian smoothing sigma.
+
+    Returns:
+        np.ndarray: Binary image
     """
 
     # Smooth
@@ -51,15 +43,13 @@ def thresh_slide(gray, thresh_val, sigma=13):
 
 
 def fill_tissue_holes(bw_img):
-    """ Filling holes in tissue image
-    Parameters
-    ----------
-    bw_img : np.array
-        2D binary image.
-    Returns
-    -------
-    bw_fill: np.array
-        Binary image with no holes
+    """ Filling holes in tissue image.
+
+    Args:
+        bw_img (np.ndarray): 2D binary image.
+
+    Returns:
+        np.ndarray: Binary image with no holes
     """
 
     # Fill holes
@@ -69,38 +59,45 @@ def fill_tissue_holes(bw_img):
 
 
 def remove_small_tissue(bw_img, min_size=10000):
-    """ Remove small holes in tissue image
-    Parameters
-    ----------
-    bw_img : np.array
-        2D binary image.
-    min_size: int
-        Minimum tissue area.
-    Returns
-    -------
-    bw_remove: np.array
-        Binary image with small tissue regions removed
+    """Remove small holes in tissue image.
+
+    Args:
+        bw_img (np.ndarray): 2D binary image.
+        min_size (int): Minimum tissue area.
+
+    Returns:
+        np.ndarray: Binary image with small tissue regions removed.
+
+    Raises:
+        ValueError: If `bw_img` is not a 2D numpy array or `min_size` is not a positive integer.
     """
 
-    bw_remove = remove_small_objects(bw_img, min_size=min_size, connectivity=8)
+    if not isinstance(bw_img, np.ndarray) or bw_img.ndim != 2:
+        raise ValueError("bw_img must be a 2D numpy array")
 
-    return bw_remove
+    if not isinstance(min_size, int) or min_size <= 0:
+        raise ValueError("min_size must be a positive integer")
+
+    return remove_small_objects(bw_img, min_size=min_size, connectivity=8)
 
 
 def find_tissue_cnts(bw_img):
-    """ Fint contours of tissues
-    Parameters
-    ----------
-    bw_img : np.array
-        2D binary image.
-    Returns
-    -------
-    cnts: list
-        List of all contours coordinates of tissues.
-    """
+    """Find contours of tissues.
 
-    cnts, _ = cv2.findContours(img_as_ubyte(bw_img), mode=cv2.RETR_EXTERNAL,
-                               method=cv2.CHAIN_APPROX_NONE)
+    Args:
+        bw_img (np.ndarray): 2D binary image.
+
+    Returns:
+        list: List of all contours coordinates of tissues.
+
+    Raises:
+        ValueError: If `bw_img` is not a 2D numpy array.
+    """
+    if not isinstance(bw_img, np.ndarray) or bw_img.ndim != 2:
+        raise ValueError("bw_img must be a 2D numpy array")
+
+    _, cnts, _ = cv2.findContours(img_as_ubyte(bw_img), mode=cv2.RETR_CCOMP,
+                                  method=cv2.CHAIN_APPROX_NONE)
 
     return cnts
 
@@ -108,44 +105,49 @@ def find_tissue_cnts(bw_img):
 def locate_tissue_cnts(slide_path,
                        max_img_size=2048,
                        smooth_sigma=13,
-                       thresh_val = 0.80,
+                       thresh_val=0.80,
                        min_tissue_size=10000):
-    """ Locate tissue contours of whole slide image
-    Parameters
-    ----------
-    slide_path : valid slide path
-        The slide to locate the tissue.
-    max_img_size: int
-        Max height and width for the size of slide with selected level.
-    smooth_sigma: int
-        Gaussian smoothing sigma.
-    thresh_val: float
-        Thresholding value.
-    min_tissue_size: int
-        Minimum tissue area.
-    Returns
-    -------
-    cnts: list
-        List of all contours coordinates of tissues.
-    d_factor: int
-        Downsampling factor of selected level compared to level 0
-    """
-    # Step 1: Select the proper level
-    s_level, d_factor = select_slide_level(slide_path, max_img_size)
-    # Step 2: Load Slide image with selected level
-    slide_img = load_slide_img(slide_path, s_level)
-    # Step 3: Convert color image to gray
-    gray_img = rgb2gray(slide_img)
-    # Step 4: Smooth and Binarize
-    bw_img = thresh_slide(gray_img, thresh_val, sigma=smooth_sigma)
-    # Step 5: Fill tissue holes
-    bw_fill = fill_tissue_holes(bw_img)
-    # Step 6: Remove small tissues
-    bw_remove = remove_small_tissue(bw_fill, min_tissue_size)
-    # Step 7: Locate tissue regions
-    cnts = find_tissue_cnts(bw_remove)
+    """Locate tissue contours of whole slide image.
 
-    # slide_img = np.ascontiguousarray(slide_img, dtype=np.uint8)
-    # cv2.drawContours(slide_img, cnts, -1, (0, 255, 0), 5)
+    Args:
+        slide_path (str): Valid slide path.
+        max_img_size (int): Max height and width for the size of slide with selected level.
+        smooth_sigma (int): Gaussian smoothing sigma.
+        thresh_val (float): Thresholding value.
+        min_tissue_size (int): Minimum tissue area.
+
+    Returns:
+        tuple: A tuple containing the list of all contours coordinates of tissues and the downsampling factor of selected level compared to level 0.
+
+    Raises:
+        ValueError: If `slide_path` is not a valid string path or any of the other input parameters are invalid.
+    """
+    if not isinstance(slide_path, str) or not slide_path:
+        raise ValueError("slide_path must be a valid string path")
+
+    if not isinstance(max_img_size, int) or max_img_size <= 0:
+        raise ValueError("max_img_size must be a positive integer")
+
+    if not isinstance(smooth_sigma, int) or smooth_sigma <= 0:
+        raise ValueError("smooth_sigma must be a positive integer")
+
+    if not isinstance(thresh_val, float) or thresh_val < 0 or thresh_val > 1:
+        raise ValueError("thresh_val must be a float between 0 and 1")
+
+    if not isinstance(min_tissue_size, int) or min_tissue_size <= 0:
+        raise ValueError("min_tissue_size must be a positive integer")
+
+    with select_slide_level(slide_path, max_img_size) as (s_level, d_factor), \
+         load_slide_img(slide_path, s_level) as slide_img:
+        # Convert color image to gray
+        gray_img = rgb2gray(slide_img)
+        # Smooth and Binarize
+        bw_img = thresh_slide(gray_img, thresh_val, sigma=smooth_sigma)
+        # Fill tissue holes
+        bw_fill = fill_tissue_holes(bw_img)
+        # Remove small tissues
+        bw_remove = remove_small_tissue(bw_fill, min_tissue_size)
+        # Locate tissue regions
+        cnts = find_tissue_cnts(bw_remove)
 
     return cnts, d_factor
